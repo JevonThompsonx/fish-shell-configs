3ecae908963dfc7e48dacd97457fecf8cd03c02e
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-11 23:51:13 -0700

    Update auto_update.fish

--- functions/auto_update.fish
+++ functions/auto_update.fish
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+function auto_update
 # Notify the user that the update is starting
-notify-send "System Update" "Starting system update with yay..."
+    notify-send "System Update" "Starting system update with yay..."
 
 # Update and upgrade system using yay
-yay -Syu --noconfirm
+    yay -Syu --noconfirm
 
 # Notify the user that the update is complete
-notify-send "System Update" "System update completed!"
+    notify-send "System Update" "System update completed!"
+end 
