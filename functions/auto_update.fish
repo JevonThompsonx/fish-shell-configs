60fcf65dcd7ea436800eb7b1d89320a2a305d182
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-11 23:46:47 -0700

    Created auto_update.fish
    A script to auto update on desktop launch

---
+++ functions/auto_update.fish
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Notify the user that the update is starting
+notify-send "System Update" "Starting system update with yay..."
+
+# Update and upgrade system using yay
+yay -Syu --noconfirm
+
+# Notify the user that the update is complete
+notify-send "System Update" "System update completed!"
