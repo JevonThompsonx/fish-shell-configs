ef7e9bea6b2fb7a8d9701c55472ca1a7f23f8f75
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-11 23:56:51 -0700

    Update auto_update.fish
    Needs to be a auto load thing not a function

--- functions/auto_update.fish
+++ functions/auto_update.fish
@@ -1,10 +1,9 @@
-function auto_update
+#!/bin/bash
 # Notify the user that the update is starting
-    notify-send "System Update" "Starting system update with yay..."
+notify-send "System Update" "Starting system update with yay..."
 
 # Update and upgrade system using yay
-    yay -Syu --noconfirm
+yay -Syu --noconfirm
 
 # Notify the user that the update is complete
-    notify-send "System Update" "System update completed!"
-end 
+notify-send "System Update" "System update completed!"
