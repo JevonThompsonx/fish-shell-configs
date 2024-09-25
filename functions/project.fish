f9cbf8b13321bfabfa950561b4ef9336a325b823
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-24 20:59:17 -0400

    updated project function
    - Goes into specified directory if one is given
    - Defaults to project directory if none is given

--- functions/project.fish
+++ functions/project.fish
@@ -1,4 +1,16 @@
 function project 
-  cd ~/Documents/Projects
+
+# Check if an argument is provided
+if test (count $argv) -gt 0
+    # If the argument is a valid directory, change to it
+    if test -d $argv[1]
+        cd ~/Documents/Projects/$argv[1]
+    else
+        echo "Error: '$argv[1]' is not a valid directory."
+    end
+else
+    # Default directory
+    cd ~/Documents/Projects
+end
   nvim
 end
