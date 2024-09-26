0f0a447007595bac0fcbaa89362baebadcd263fd
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-25 19:38:27 -0700

    create w/b bun scripts

---
+++ functions/bCreateReact.fish
@@ -0,0 +1,17 @@
+function bCreateReact
+  set projectName $argv[1]
+  
+  if test -n "$projectName"
+    echo "Cloning..."
+    git clone https://github.com/JevonThompsonx/react_ts_template.git
+    mv react_ts_template $projectName
+    cd $projectName
+    bun install
+    echo "Done!"
+    echo "Remember to change project name in package.json"
+    echo "Happy coding!!"
+  else
+    echo "Error! You probably didn't pass in a project name. Try again."
+  end
+end
+
